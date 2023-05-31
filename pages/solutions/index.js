a42baa9819077aaffb94071d765cb35859044a20
Dan Cabaniss <dccabs@gmail.com>
2023-05-31 13:56:14 -0500

    added in fetch to api

--- pages/solutions/index.js
+++ pages/solutions/index.js
@@ -6,27 +6,32 @@ import RecentActivityTable from "/components/RecentActivityTable";
 import RecentClients from "/components/RecentClients";
 import DynamicComponent from "/components/DynamicComponent";
 
-export default function Solutions() {
+export default function Solutions(props) {
+  const config = props.config;
+  const getScope = (module) => {
+    const match = config.find((item) => item.component === module);
+    return match.remote_name
+  }
   return (
     <Layout>
-      <DynamicComponent module="header" scope="remote1">
+      <DynamicComponent module="header" scope={getScope('header')}>
         <Header />
       </DynamicComponent>
       <main>
         <div className="relative isolate overflow-hidden pt-16">
-          <DynamicComponent module="secondNav" scope="remote1">
+          <DynamicComponent module="secondNav" scope={getScope('secondNav')}>
             <SecondNav />
           </DynamicComponent>
-          <DynamicComponent module="stats" scope="remote1">
+          <DynamicComponent module="stats" scope={getScope('stats')}>
             <Stats />
           </DynamicComponent>
         </div>
 
         <div className="space-y-16 py-16 xl:space-y-20">
-          <DynamicComponent module="recentActivity" scope="remote1">
+          <DynamicComponent module="recentActivity" scope={getScope('recentActivity')}>
             <RecentActivityTable />
           </DynamicComponent>
-          <DynamicComponent module="recentClients" scope="remote1">
+          <DynamicComponent module="recentClients" scope={getScope('recentClients')}>
             <RecentClients />
           </DynamicComponent>
         </div>
@@ -34,3 +39,9 @@ export default function Solutions() {
     </Layout>
   );
 }
+
+export const getServerSideProps = async () => {
+  const res = await fetch('https://next-mf-api.vercel.app/api/getConfig');
+  const config = await res.json();
+  return { props: { config } };
+};
